4216622332ac2ad90d37f3ea3fce284b9e85d027
Jeff Buchbinder <jeff@ourexchange.net>
2010-08-17 17:31:10 +0000

    More or less working Topaz tablet support using SigPlus java library. Tested with T-S751-HSB model as baseline.

--- shim-drivers/shim-driver-topaz/src/main/java/org/freemedsoftware/device/impl/SignatureTopazShim.java
+++ shim-drivers/shim-driver-topaz/src/main/java/org/freemedsoftware/device/impl/SignatureTopazShim.java
@@ -24,11 +24,18 @@
 
 package org.freemedsoftware.device.impl;
 
-import gnu.io.CommDriver;
-
+import java.awt.image.BufferedImage;
 import java.beans.Beans;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
 
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.freemedsoftware.device.DeviceCapability;
 import org.freemedsoftware.device.ShimDevice;
@@ -48,18 +55,117 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 
 	protected String currentJobId = null;
 
+	/**
+	 * Amount of time in milliseconds to wait between signature polling.
+	 */
+	protected final static int SIGNATURE_WAIT_DURATION = 500;
+
+	/**
+	 * Maximum amount of time to wait after last signature point recorded.
+	 */
+	protected final static int SIGNATURE_MAXIMUM_WAIT = 2000;
+
+	/**
+	 * Wait this much time in milliseconds before giving up.
+	 */
+	protected final static int SIGNATURE_INITIAL_WAIT = 15000;
+
+	protected final static int SIGNATURE_MINIMUM_POINTS = 10;
+
+	protected static Timer timer = new Timer();
+
+	protected static AtomicLong lastSeen = new AtomicLong();
+
+	protected static long requestTime = 0L;
+
+	protected static int numSigPoints = 0;
+
+	protected class TopazTimerTask extends TimerTask {
+
+		@Override
+		public void run() {
+			log.info("TopazTimerTask running");
+
+			// If we have no points at all ...
+			if (sigObj.getSignatureData().m_PointArray.length == 0) {
+				// ... and we have been waiting for less than
+				// SIGNATURE_INITIAL_WAIT ms
+				if ((System.currentTimeMillis() - requestTime) < SIGNATURE_INITIAL_WAIT) {
+					log.debug("No points seen yet, waiting for about "
+							+ (SIGNATURE_INITIAL_WAIT - (System
+									.currentTimeMillis() - requestTime))
+							+ " ms before giving up");
+
+					// Go back to waiting.
+					return;
+				} else {
+					log.warn("Failed to see any points after "
+							+ (System.currentTimeMillis() - requestTime)
+							+ " ms, aborting.");
+
+					// Clear everything
+					log.info("Clearing signature job id");
+					currentJobId = null;
+					lastSeen.set(0L);
+					log.info("Clearing tablet.");
+					sigObj.setEnabled(false);
+					sigObj.setTabletState(0);
+					sigObj.clearTablet();
+
+					log.info("Cancelling timer.");
+					timer.cancel();
+				}
+			}
+
+			log.debug("numSigPoints = " + numSigPoints
+					+ " / sigObj.getSignatureData().m_PointArray.length = "
+					+ sigObj.getSignatureData().m_PointArray.length);
+			if (sigObj.getSignatureData().m_PointArray.length > numSigPoints) {
+				// Bump up when an event was last seen to now.
+				lastSeen.set(System.currentTimeMillis());
+
+				// ... and increase the counter
+				numSigPoints = sigObj.getSignatureData().m_PointArray.length;
+
+				// Wait until next task execution
+				log
+						.debug("New points seen since last polling, dropping back into loop.");
+				return;
+			}
+
+			// Test to see if anything is there
+			if (sigObj.getSignatureData().m_PointArray.length < SIGNATURE_MINIMUM_POINTS) {
+				// Wait until next task execution
+				log.debug("Not enough points recorded, back to waiting");
+				return;
+			}
+
+			// If everything else works properly, we're going to deal with the
+			// data we've got.
+
+			// "Handle" the signature
+			handleSignature();
+
+			// Clear everything
+			log.info("Clearing signature job id");
+			currentJobId = null;
+			lastSeen.set(0L);
+			log.info("Clearing pad.");
+			sigObj.setEnabled(false);
+			sigObj.setTabletState(0);
+			sigObj.clearTablet();
+
+			log.info("Cancelling timer.");
+			timer.cancel();
+		}
+	}
+
 	@Override
 	public void configure(HashMap<String, Object> config) {
 	}
 
 	@Override
 	public void init() throws Exception {
-		// Initialize rxtx communications driver
-		String drivername = "gnu.io.CommDriver";
-		CommDriver driver = (CommDriver) Class.forName(drivername)
-				.newInstance();
-		driver.initialize();
-
 		// Load Topaz SigPlus driver on top of rxtx
 		ClassLoader cl = (SigPlus.class).getClassLoader();
 		sigObj = (SigPlus) Beans.instantiate(cl, "com.topaz.sigplus.SigPlus");
@@ -68,8 +174,8 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 		sigObj.clearTablet();
 
 		// TODO: FIXME: Need to pull from actual configuration
-		sigObj.setTabletModel("SignatureGem1X5");
-		sigObj.setTabletComPort("COM1");
+		sigObj.setTabletModel("SignatureGem4X5");
+		sigObj.setTabletComPort("HID1"); // HID1 == "HSB" tablet HID port
 
 		// Attach event listener
 		sigObj.addSigPlusListener(this);
@@ -81,6 +187,8 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 	@Override
 	public void close() throws Exception {
 		if (sigObj != null) {
+			sigObj.setEnabled(false);
+			sigObj.setTabletState(0);
 			sigObj.clearTablet();
 		}
 	}
@@ -93,17 +201,25 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 			return false;
 		}
 
+		// Record the request time
+		requestTime = System.currentTimeMillis();
+
 		// Clear tablet
 		sigObj.clearTablet();
 
 		// Store uid locally
 		currentJobId = uid;
 
+		sigObj.setEnabled(true);
 		sigObj.autoKeyStart();
 		sigObj.setAutoKeyData("Sample Encryption Data");
 		sigObj.autoKeyFinish();
 		sigObj.setEncryptionMode(2);
+		sigObj.setSavePressureData(true);
 		sigObj.setSigCompressionMode(1);
+
+		// Enable "capture" mode
+		sigObj.setTabletState(1);
 
 		// Attempt to readjust LCD on models which support it
 		try {
@@ -114,6 +230,11 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 			log.info("LCD adjustment failed with " + ex.toString());
 		}
 
+		// Schedule a timer task to attempt to poll the pad
+		lastSeen.set(System.currentTimeMillis());
+		timer.schedule(new TopazTimerTask(), SIGNATURE_WAIT_DURATION,
+				SIGNATURE_WAIT_DURATION);
+
 		return true;
 	}
 
@@ -121,10 +242,15 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 	}
 
 	public void handleNewTabletData(SigPlusEvent0 event) {
+		// Update the record of when an event was last seen.
+		lastSeen.set(System.currentTimeMillis());
+
 		if (currentJobId == null) {
 			log.error("No current job id, unexpected data");
 		}
+	}
 
+	protected void handleSignature() {
 		// mySigString = sigObj.getSigString();
 		sigObj.setSigCompressionMode(0);
 		sigObj.setEncryptionMode(0);
@@ -139,10 +265,54 @@ public class SignatureTopazShim implements SignatureInterface, SigPlusListener {
 		}
 
 		TopazSigCapData sigData = sigObj.getSignatureData();
+		log.trace(sigData.m_byRawData);
+
 		// TODO: store data
+		log.info("Storing data.");
+
+		// Create PNG image
+		sigObj.setImageJustifyMode(5);
+		sigObj.setImagePenWidth(10);
+		sigObj.setImageXSize(1000);
+		sigObj.setImageYSize(350);
+		BufferedImage sigImage = sigObj.sigImage();
+		int w = sigImage.getWidth(null);
+		int h = sigImage.getHeight(null);
+		int[] pixels = new int[(w * h) * 2];
+		sigImage.setRGB(0, 0, 0, 0, pixels, 0, 0);
+		ByteArrayOutputStream fos = new ByteArrayOutputStream();
+		try {
+			ImageIO.write(sigImage, "png", fos);
+		} catch (IOException ex) {
+			log.error(ex);
+		}
+		try {
+			fos.close();
+		} catch (IOException ex) {
+			log.error(ex);
+		}
+
+		// Disable tablet capture mode
+		sigObj.setTabletState(0);
+
 	}
 
 	public void handleTabletTimerEvent(SigPlusEvent0 event) {
+	}
+
+	/**
+	 * Main method for testing only
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		BasicConfigurator.configure();
+		SignatureTopazShim s = new SignatureTopazShim();
+		System.out.println("Initializing shim driver");
+		s.init();
+		System.out.println("Initializing signature request for pad");
+		s.initSignatureRequest("12345");
 	}
 
 }

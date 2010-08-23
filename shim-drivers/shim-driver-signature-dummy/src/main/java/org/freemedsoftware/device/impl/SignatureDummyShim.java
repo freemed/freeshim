4c3eccfcccf5141f6c8641e467b9d8f038f21f7c
Jeff Buchbinder <jeff@ourexchange.net>
2010-08-23 00:40:59 +0000

    Implement more of the dummy signature driver for testing the webapp.

--- shim-drivers/shim-driver-signature-dummy/src/main/java/org/freemedsoftware/device/impl/SignatureDummyShim.java
+++ shim-drivers/shim-driver-signature-dummy/src/main/java/org/freemedsoftware/device/impl/SignatureDummyShim.java
@@ -26,17 +26,47 @@ package org.freemedsoftware.device.impl;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 import org.freemedsoftware.device.DeviceCapability;
 import org.freemedsoftware.device.JobStoreItem;
+import org.freemedsoftware.device.PersistentJobStoreDAO;
 import org.freemedsoftware.device.ShimDevice;
 import org.freemedsoftware.device.SignatureInterface;
+import org.tmatesoft.sqljet.core.SqlJetException;
 
 @ShimDevice(name = "Signature Dummy Shim", capability = DeviceCapability.DEVICE_SIGNATURE_TABLET)
 public class SignatureDummyShim implements SignatureInterface {
 
 	protected Logger log = Logger.getLogger(DummyShim.class);
+
+	protected Timer timer = new Timer();
+
+	protected Integer jobId = null;
+
+	protected JobStoreItem item = null;
+
+	public final static int PROCESSING_LENGTH = 5000;
+
+	protected class ProcessingTimerTask extends TimerTask {
+
+		@Override
+		public void run() {
+			item.setStatus(JobStoreItem.STATUS_COMPLETED);
+			try {
+				PersistentJobStoreDAO.update(item);
+			} catch (SqlJetException e) {
+				log.error(e);
+			}
+
+			// Clear job and item objects
+			jobId = null;
+			item = null;
+		}
+
+	}
 
 	@Override
 	public void init() {
@@ -45,12 +75,25 @@ public class SignatureDummyShim implements SignatureInterface {
 
 	@Override
 	public boolean initSignatureRequest(JobStoreItem item) throws Exception {
-		return false;
+		jobId = item.getId();
+
+		// Update status to "PENDING" (processing)
+		item.setStatus(JobStoreItem.STATUS_PENDING);
+		try {
+			PersistentJobStoreDAO.update(item);
+		} catch (SqlJetException e) {
+			log.error(e);
+		}
+
+		this.item = item;
+		timer.schedule(new ProcessingTimerTask(), PROCESSING_LENGTH);
+		return true;
 	}
 
 	@Override
 	public void close() throws Exception {
 		log.info("close dummy sig device");
+		timer.cancel();
 	}
 
 	@Override
@@ -65,7 +108,7 @@ public class SignatureDummyShim implements SignatureInterface {
 
 	@Override
 	public boolean isProcessing() {
-		return false;
+		return (jobId != null);
 	}
 
 }

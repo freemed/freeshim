335dac0581118f37304a327193120bebcbcee717
Jeff Buchbinder <jeff@ourexchange.net>
2010-08-24 20:32:55 +0000

    Add skeleton framework for ESC-POS based label printers, no functionality yet.

---
+++ shim-drivers/shim-driver-label-escpos/src/main/java/org/freemedsoftware/device/impl/LabelEscPosShim.java
@@ -0,0 +1,116 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *
+ * FreeMED Electronic Medical Record / Practice Management System
+ * Copyright (C) 1999-2010 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.freemedsoftware.device.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+import org.freemedsoftware.device.DeviceCapability;
+import org.freemedsoftware.device.JobStoreItem;
+import org.freemedsoftware.device.PersistentJobStoreDAO;
+import org.freemedsoftware.device.ShimDevice;
+import org.freemedsoftware.device.SignatureInterface;
+import org.tmatesoft.sqljet.core.SqlJetException;
+
+@ShimDevice(name = "Label Printer ESC-POS Shim", capability = DeviceCapability.DEVICE_LABEL_PRINTER)
+public class LabelEscPosShim implements SignatureInterface {
+
+	protected Logger log = Logger.getLogger(LabelEscPosShim.class);
+
+	protected Timer timer = new Timer();
+
+	protected Integer jobId = null;
+
+	protected JobStoreItem item = null;
+
+	public final static int PROCESSING_LENGTH = 5000;
+
+	protected HashMap<String, Object> config = new HashMap<String, Object>();
+
+	protected class ProcessingTimerTask extends TimerTask {
+
+		@Override
+		public void run() {
+			item.setStatus(JobStoreItem.STATUS_COMPLETED);
+			try {
+				PersistentJobStoreDAO.update(item);
+			} catch (SqlJetException e) {
+				log.error(e);
+			}
+
+			// Clear job and item objects
+			jobId = null;
+			item = null;
+		}
+
+	}
+
+	@Override
+	public void init() {
+		log.info("Open label printer device");
+	}
+
+	@Override
+	public boolean initJobRequest(JobStoreItem item) throws Exception {
+		jobId = item.getId();
+
+		// Update status to "PENDING" (processing)
+		item.setStatus(JobStoreItem.STATUS_PENDING);
+		try {
+			PersistentJobStoreDAO.update(item);
+		} catch (SqlJetException e) {
+			log.error(e);
+		}
+
+		this.item = item;
+		timer.schedule(new ProcessingTimerTask(), PROCESSING_LENGTH);
+		return true;
+	}
+
+	@Override
+	public void close() throws Exception {
+		log.info("Close label printer");
+		timer.cancel();
+	}
+
+	@Override
+	public void configure(HashMap<String, Object> config) {
+		this.config = config;
+	}
+
+	@Override
+	public List<String> getConfigurationOptions() {
+		return null;
+	}
+
+	@Override
+	public boolean isProcessing() {
+		return (jobId != null);
+	}
+
+}

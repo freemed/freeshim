dc7fdaac6fe48103038d7dbca33e45f80459c7e7
Jeff Buchbinder <jeff@ourexchange.net>
2010-08-26 14:32:40 +0000

    ESC/POS fixes, second half of last commit

--- shim-drivers/shim-driver-label-escpos/src/main/java/org/freemedsoftware/device/impl/LabelEscPosShim.java
+++ shim-drivers/shim-driver-label-escpos/src/main/java/org/freemedsoftware/device/impl/LabelEscPosShim.java
@@ -24,18 +24,29 @@
 
 package org.freemedsoftware.device.impl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 import org.freemedsoftware.device.DeviceCapability;
 import org.freemedsoftware.device.JobStoreItem;
+import org.freemedsoftware.device.ParallelPrinterInterface;
 import org.freemedsoftware.device.PersistentJobStoreDAO;
 import org.freemedsoftware.device.ShimDevice;
 import org.freemedsoftware.device.SignatureInterface;
 import org.tmatesoft.sqljet.core.SqlJetException;
+
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 @ShimDevice(name = "Label Printer ESC-POS Shim", capability = DeviceCapability.DEVICE_LABEL_PRINTER)
 public class LabelEscPosShim implements SignatureInterface {
@@ -51,6 +62,8 @@ public class LabelEscPosShim implements SignatureInterface {
 	public final static int PROCESSING_LENGTH = 5000;
 
 	protected HashMap<String, Object> config = new HashMap<String, Object>();
+
+	protected ParallelPrinterInterface printerInterface = null;
 
 	protected class ProcessingTimerTask extends TimerTask {
 
@@ -71,8 +84,23 @@ public class LabelEscPosShim implements SignatureInterface {
 	}
 
 	@Override
-	public void init() {
+	public void init() throws Exception {
 		log.info("Open label printer device");
+
+		if (config == null) {
+			log.error("Config has not been set yet! Bombing out.");
+			return;
+		}
+
+		String portName = (String) config.get("escpos.port");
+		Integer timeout = Integer.parseInt((String) config
+				.get("escpos.timeout"));
+
+		log.info("Creating interface for " + portName + " with timeout "
+				+ timeout);
+
+		printerInterface = new ParallelPrinterInterface();
+		printerInterface.open(portName, timeout);
 	}
 
 	@Override
@@ -88,7 +116,15 @@ public class LabelEscPosShim implements SignatureInterface {
 		}
 
 		this.item = item;
-		timer.schedule(new ProcessingTimerTask(), PROCESSING_LENGTH);
+
+		// Process item
+		printLabel(item.getPrintTemplate(), item.getPrintParameters(), item
+				.getPrintCount());
+
+		// Clear everything
+		this.item = null;
+		this.jobId = null;
+
 		return true;
 	}
 
@@ -96,6 +132,9 @@ public class LabelEscPosShim implements SignatureInterface {
 	public void close() throws Exception {
 		log.info("Close label printer");
 		timer.cancel();
+		if (printerInterface != null) {
+			printerInterface.close();
+		}
 	}
 
 	@Override
@@ -111,6 +150,52 @@ public class LabelEscPosShim implements SignatureInterface {
 	@Override
 	public boolean isProcessing() {
 		return (jobId != null);
+	}
+
+	protected int printLabel(String printTemplate, Map<String, String> values,
+			Integer copies) throws IOException {
+		// Get template configuration
+		freemarker.template.Configuration cfg = this
+				.getFreemarkerConfiguration();
+
+		// Set values
+		if (values == null) {
+			values = new HashMap<String, String>();
+		}
+		values.put("esc", "\u001b");
+
+		Template template = cfg.getTemplate(printTemplate);
+
+		// Write template
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		Writer out = new OutputStreamWriter(os);
+		for (int iter = 0; iter < copies; iter++) {
+			try {
+				log.info("Writing label copy " + (iter + 1) + " of " + copies);
+				template.process(values, out);
+				os.flush();
+				log.debug("Got " + os.size() + " bytes");
+				printerInterface.write(os.toString());
+				log.trace("LABEL: " + os.toString());
+				log.info("Write to printer device completed.");
+				out.flush();
+			} catch (TemplateException e) {
+				log.error(e);
+				return 0;
+			}
+		}
+
+		// Return successful print
+		return 1;
+	}
+
+	protected freemarker.template.Configuration getFreemarkerConfiguration()
+			throws IOException {
+		freemarker.template.Configuration cfg = new freemarker.template.Configuration();
+		cfg.setDirectoryForTemplateLoading(new File(this.getClass()
+				.getClassLoader().getResource("/templates").toExternalForm()));
+		cfg.setObjectWrapper(new DefaultObjectWrapper());
+		return cfg;
 	}
 
 }
--- shim-drivers/shim-driver-topaz/install-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn install:install-file -DgroupId=commapi -DartifactId=commapi -Dversion=2.0.3 -Dpackaging=jar -Dfile=deps/commapi-2.0.3.jar
-mvn install:install-file -DgroupId=sigplus -DartifactId=sigplus -Dversion=2.52 -Dpackaging=jar -Dfile=deps/sigplus-2.52.jar
-

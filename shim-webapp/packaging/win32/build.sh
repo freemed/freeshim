cd16c835b22cfa0490c53fda959813fc1fa530d6
Jeff Buchbinder <jeff@ourexchange.net>
2010-09-11 19:01:00 +0000

    Add NSIS packaging, which is built under Wine.

---
+++ shim-webapp/packaging/win32/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TOMCAT_VERSION=6.0.29
+P="$( cd "$(dirname "$0")"; pwd )"
+
+cd "$P"
+
+if [ ! -f ../../target/shim.war ] ; then
+	echo "Create shim.war"
+	( cd ../.. ; mvn package || exit 1 )
+fi
+
+if [ ! -f apache-tomcat-${TOMCAT_VERSION}-windows-x86.zip ] ; then
+	echo "Grabbing tomcat ${TOMCAT_VERSION} binary package"
+	wget -c http://apache.ziply.com/tomcat/tomcat-6/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}-windows-x86.zip
+fi
+
+if [ ! -d apache-tomcat-${TOMCAT_VERSION} ]; then
+	echo "Decompression tomcat archive"
+	unzip apache-tomcat-${TOMCAT_VERSION}-windows-x86.zip
+fi
+
+echo "Building package"
+~/.wine/drive_c/Program\ Files/NSIS/makensis.exe shim.nsi
+

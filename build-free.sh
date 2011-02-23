0ff27680a2783691fb5086074012253ac4de0470
Jeff Buchbinder <jeff@ourexchange.net>
2011-02-23 19:08:30 +0000

    Update build script to build "free" version properly.

--- build-free.sh
+++ build-free.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
+#
+#	$Id$
+#	jeff@freemedsoftware.org
+#
 
+# Build a Debian "free" version of FreeSHIM.
+
+echo "Building changelog ... "
+./svn2cl.sh
+
+echo "Exporting from Subversion ... "
 svn export https://svn.master.freemedsoftware.org/freemed-utilities/shim/trunk BUILD
 
-(
-cd BUILD
+echo "Removing non-free component(s) ... "
+xsltproc remove-nonfree.xsl BUILD/shim-webapp/pom.xml > BUILD/shim-webapp/pom.xml.tmp
+mv BUILD/shim-webapp/pom.xml.tmp BUILD/shim-webapp/pom.xml -f
+xsltproc remove-nonfree.xsl BUILD/shim-drivers/pom.xml > BUILD/shim-drivers/pom.xml.tmp
+mv BUILD/shim-drivers/pom.xml.tmp BUILD/shim-drivers/pom.xml -f
+rm -rf BUILD/shim-drivers/shim-driver-signature-topaz
 
-xsltproc remove-nonfree.xsl shim-webapp/pom.xml > shim-webapp/pom.xml
-xsltproc remove-nonfree.xsl shim-drivers/pom.xml > shim-drivers/pom.xml
-rm -rf shim-drivers/
+echo "Moving in changelog ... "
+mv ChangeLog BUILD/ -vf
+
+echo "Creating source archives"
+
+VERSION=$( cat project.properties | grep 'shim.version' | cut -d= -f2 )
+echo "Version $VERSION"
+
+cp BUILD shim-${VERSION} -Rf
+
+zip -r shim-free-${VERSION}-src.zip shim-${VERSION}
+tar czvf shim-free-${VERSION}-src.tar.gz shim-${VERSION}
+rm -rf shim-${VERSION}
+
+(
+
+cd BUILD
 
 echo "Building!"
 mvn
 
-ls -1 shim-* | while read X; do
+ls -1 shim-*.*
+
+ls -1 shim-*.* | while read X; do
 	Y="${X/shim/shim-free}"
 	mv "$X" "$Y" -vf
 	mv "$Y" .. -vf
